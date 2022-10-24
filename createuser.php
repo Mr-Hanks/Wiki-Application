0b2ed5b87f7eab994d108a215de0afa801b4010a
Collin Hanks <collinnhankss@gmail.com>
2022-10-24 15:11:19 -0500

    Update

--- createuser.php
+++ createuser.php
@@ -37,7 +37,6 @@
                 <h1>Registration</h1>
                 <form name="registration" action="" method="post">
                     <input type="text" name="username" placeholder="Username" required />
-                    <input type="email" name="email" placeholder="Email" required />
                     <input type="password" name="password" placeholder="Password" required />
                     <input type="submit" name="submit" value="Register" />
                 </form>
